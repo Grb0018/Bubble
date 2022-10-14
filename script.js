44312991c325e5d43e188dd31fd2af8fde1938bf
Grb0018 <105971445+Grb0018@users.noreply.github.com>
2022-10-14 12:41:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ var StartX =0;
 var StartY = 0;
 var drag;
 var siteChange = false;
+var click = true;
+
+
 document.body.onload =(e)=>{
-    $("#game").fadeIn(1500)
+    $("#game").fadeIn(100)
 }
 
 document.body.onclick=(e)=>{
@@ -17,39 +20,41 @@ document.body.onclick=(e)=>{
         $('#play').fadeOut('800')
         $('.text').fadeOut('800')
         $('#playground').fadeIn('800')
-        openFullscreen()
         var a = document.getElementsByClassName('ball')
-for (let i = 0; i < a.length; i++) {
-    setTimeout(()=>{a[i].style.animation = '9000ms water ease-in infinite'},i*200)
-}
-    }
+        if(click==true){
+            click=false;
+            for (let i = 0; i < a.length; i++) {
+                setTimeout(()=>{a[i].style.animation = '9000ms water ease-in infinite'},i*200)
+            }
+        }
+        document.getElementById('anchor').style.display='block'
+        document.getElementById('anchor').style.animation = '500ms anchordown linear';
+        setTimeout(()=>{document.body.style.animation = 'shak 200ms linear'},700);
+        setTimeout(()=>{
+            for (let i = 0; i < 12; i++) {
+                particlex(37,16,'url(./balls/bubble.png)','game') 
+            }
+        },800)
+       
+        
+        
+
+
+document.body.onmousemove = (event) =>{bubble(event)}
+document.body.onmousedown=(e)=>{
     var allBall= document.getElementsByClassName('ball');
     for(const x of allBall){
         if(e.target == x){
             siteChange = true;
-            console.log(parseInt($(e.target).css('border-radius')))
-            if(parseInt($(e.target).css('border-radius')) >0){
-                siteChange = false;
-                $(e.target).css({
-                    'border': 'none',
-                    'border-radius': '0'
-                })
-            }
             StartX = e.clientX;
             StartY = e.clientY;
-            if(siteChange == true){
-                $(x).css({
-                    'border': '0.2vw solid white',
-                    'border-radius': '2.5vw'
-                })
-            }
                 document.body.onmousemove = (e)=>{
                     var FinalX = e.clientX;
                     var FinalY = e.clientY;
                     console.log(((StartX-FinalX)/screen.availWidth)*100 )
                     if(siteChange == true){
                     /////////LEFT ///////////////
-                  if(StartX-FinalX > (2.8*vw) && StartX-FinalX < (5.5*vw)){
+                  if(StartX-FinalX > (2*vw) && StartX-FinalX < (5.5*vw)){
                     if(StartY-FinalY > (-1*vw) && StartY-FinalY < (1.2*vw)){
                         left()
                         document.body.onmousemove = (event) =>{bubble(event)};
@@ -92,42 +97,23 @@ for (let i = 0; i < a.length; i++) {
                         })
                     }
                 } 
+                item = x;
+                return;
                    }
                 }
+            }
+    }
+}  
+    }               
 
-               
-               
-            
-            $('#pad').css({opacity:'1'})
-            var allimgPad = document.getElementById('pad').querySelectorAll('img');
-            allimgPad.forEach(element => {
-            element.style.animation='do 1200ms linear infinite';
-        });
-            item = x;
-            return;
+           
         } 
           
-    }
-}
-function padReset(){
-    $('#pad').css({opacity:'0.5'})
-        var allimgPad = document.getElementById('pad').querySelectorAll('img');
-        allimgPad.forEach(element => {
-            element.style.animation='none';
-        });
-        var allBalls= document.getElementsByClassName('ball');
-        for (let y of allBalls) {
-            $(y).css({
-                        filter:'brightness(0.92)',
-                        opacity:'1'
-                })
-        }
-}
 function right(){
-    padReset()
+    
     var allitem = document.body.querySelectorAll('item')    
 var no = parseInt($(item).attr('no'))
-var rowNo = parseInt($(item).attr('rown'))
+var rown = parseInt($(item).attr('rown'))
 var coln = parseInt($(item).attr('coln'))
 if(no != 7 && no != 15 && no != 23 && no != 31 && no != 39 && no != 47) {
     for (const x of allitem) {
@@ -140,19 +126,25 @@ if(no != 7 && no != 15 && no != 23 && no != 31 && no != 39 && no != 47) {
     $(item).animate({
         left: Riteml,
         top: Ritemt
-    },350)
+    },120)
     $(x).animate({
         left: iteml,
         top: itemt
-    },350)
+    },120)
     $(item).attr('no',(no+1))
     $(x).attr('no',no)
     $(item).attr('coln',(coln+1))
     $(x).attr('coln',coln)
-verChk(rowNo)
-            colChk(coln)
-            colChk(coln+1);
-            setTimeout(()=>{downfall()},1900)
+    chk(rown)
+    colChk(coln)
+    colChk(coln+1)
+    // setTimeout(()=>{verChk(rown)},150)
+    // setTimeout(()=>{colChk(coln)},150)
+    // setTimeout(()=>{colChk(coln+1)},150)
+    // if(coln<7 && coln>0){
+    //     setTimeout(()=>{colChk(coln-1)},150)
+    // }
+     setTimeout(()=>{downfall()},800)
     return;
     
         } 
@@ -161,7 +153,7 @@ verChk(rowNo)
   
 }
 function left(){
-    padReset()
+    
     var allitem = document.body.querySelectorAll('item')    
 var no = parseInt($(item).attr('no'))
 var rown = parseInt($(item).attr('rown'))
@@ -177,19 +169,27 @@ if(no != 0 && no != 8 && no != 16 && no != 24 && no != 32 && no != 40) {
             $(item).animate({
                 left: Riteml,
                 top: Ritemt
-            },350)
+            },120)
             $(x).animate({
                 left: iteml,
                 top: itemt
-            },350)
+            },120)
             $(item).attr('no',(no-1))
             $(x).attr('no',no)
             $(item).attr('coln',(coln-1))
             $(x).attr('coln',coln)
-            verChk(rown)
+            setTimeout(()=>{
+                chk(rown)
             colChk(coln)
-            colChk(coln-1);
-            setTimeout(()=>{downfall()},1900)
+            colChk(coln-1)
+            },150)
+            
+            // setTimeout(()=>{colChk(coln)},150)
+            // if(coln<7 && coln>0){
+            //     setTimeout(()=>{colChk(coln+1)},150)
+            // }
+            // setTimeout(()=>{colChk(coln-1)},150)
+             setTimeout(()=>{downfall()},800)
             return;
         } 
     }
@@ -197,7 +197,7 @@ if(no != 0 && no != 8 && no != 16 && no != 24 && no != 32 && no != 40) {
 }   
 }
 function up(){
-    padReset()
+    
     var allitem = document.body.querySelectorAll('item')    
 var no = parseInt($(item).attr('no'));
 var rown = parseInt($(item).attr('rown'))
@@ -213,19 +213,25 @@ if(no != 0 && no != 1 && no != 2 && no != 3 && no != 4 && no != 5 && no != 6 && 
             $(item).animate({
                 left: Riteml,
                 top: Ritemt
-            },350)
+            },120)
             $(x).animate({
                 left: iteml,
                 top: itemt
-            },350)
+            },120)
             $(item).attr('no',(no-8))
             $(x).attr('no',no)
             $(item).attr('rown',(rown-1))
             $(x).attr('rown',rown)
-            verChk(rown)
-            verChk(rown-1)
+            //  setTimeout(()=>{verChk(rown)},150)
+            // setTimeout(()=>{colChk(coln)},150)
+            // setTimeout(()=>{verChk(rown-1)},150)
+            // if(rown<5 && rown>0){
+            //     setTimeout(()=>{verChk(rown+1)},150)
+            // }
+            setTimeout(()=>{downfall()},800)
+            chk(rown)
+            chk(rown-1)
             colChk(coln)
-            setTimeout(()=>{downfall()},1900)
             return;
         } 
     }
@@ -233,7 +239,7 @@ if(no != 0 && no != 1 && no != 2 && no != 3 && no != 4 && no != 5 && no != 6 && 
 }   
 }
 function down(){
-    padReset()
+    
     var allitem = document.body.querySelectorAll('item')    
 var no = parseInt($(item).attr('no'))
 var rown = parseInt($(item).attr('rown'))
@@ -249,19 +255,26 @@ if(no != 40 && no != 41 && no != 42 && no != 43 && no != 44 && no != 45 && no !=
             $(item).animate({
                 left: Riteml,
                 top: Ritemt
-            },350)
+            },120)
             $(x).animate({
                 left: iteml,
                 top: itemt
-            },350)
+            },120)
             $(item).attr('no',(no+8))
             $(x).attr('no',no)
             $(item).attr('rown',(rown+1))
             $(x).attr('rown',rown)
-            verChk(rown)
-            verChk(rown+1)
-            colChk(coln);
-            setTimeout(()=>{downfall()},1900)
+            console.log(rown)
+            //  setTimeout(()=>{verChk(rown)},150)
+            // setTimeout(()=>{colChk(coln)},200)
+            // setTimeout(()=>{verChk(rown-1)},250)
+            // if(rown<5 && rown>0){
+            //     setTimeout(()=>{verChk(rown+1)},350)
+            // }
+             setTimeout(()=>{downfall()},800)
+            chk(rown)
+            chk(rown+1)
+            colChk(coln)
             return;
         } 
     }
@@ -277,18 +290,13 @@ function openFullscreen() {
       elem.msRequestFullscreen();
     }
   }
-  var elem = document.body
+  var elem = document.body;
+
   
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  $('#playground').css({'margin-top':'-2.5vw'})
-}
-  
-  
-  
-  
-   document.body.ontouchstart =(e) =>{
-                   var allBall= document.getElementsByClassName('ball');
+
+
+  document.body.ontouchstart =(e) =>{
+    var allBall= document.getElementsByClassName('ball');
     for(const x of allBall){
         if(e.target==x){
             $(e.target).css({
@@ -297,61 +305,63 @@ function openFullscreen() {
     })
         }
     }
-                    StartX = e.touches[0].clientX;
-                    StartY = e.touches[0].clientY;
-                    var allBall= document.getElementsByClassName('ball');
-                    for(const x of allBall){
-                        if(e.target == x){item = x;}}
-                    document.body.ontouchmove =(ee) =>{
-                        var FinalX = ee.touches[0].clientX;
-                        var FinalY = ee.touches[0].clientY;
-                     //   console.log(((StartY-FinalY)/screen.availWidth)*100 )
-                        ///// LEFT ///////
-                        if(StartX-FinalX > (5.3*vw) && StartX-FinalX < (10.5*vw)){
-                            if(StartY-FinalY > (-1.4*vw) && StartY-FinalY < (2.2*vw)){
-                                left()
-                                document.body.ontouchmove = (event) =>{bubble(event)};
-                                $(e.target).css({
-                                    'border': 'none',
-                                    'border-radius': 'none'
-                                })
-                            } 
-                           }
-                        ///// RIGHT ///////  
-                      //  console.log(((StartX-FinalX)/screen.availWidth)*100 ) 
-                        if(StartX-FinalX > (-7*vw) && StartX-FinalX < (-3*vw)){
-                            if(StartY-FinalY > (-1.4*vw) && StartY-FinalY < (2.2*vw)){
-                                right()
-                                document.body.ontouchmove = (event) =>{bubble(event)};
-                                $(e.target).css({
-                                    'border': 'none',
-                                    'border-radius': 'none'
-                                })
-                            } 
-                           }
-                           ///// UP ///////  
-                        if(StartX-FinalX > (-0.5*vw) && StartX-FinalX < (1.2*vw)){
-                            if(StartY-FinalY > (2.5*vw) && StartY-FinalY < (7.5*vw)){
-                               up()
-                                document.body.ontouchmove = (event) =>{bubble(event)};
-                                $(e.target).css({
-                                    'border': 'none',
-                                    'border-radius': 'none'
-                                })
-                            } 
-                           }
-                        /////// DOWN ///////
-                      //  console.log(((StartY-FinalY)/screen.availWidth)*100 )
-                        if(StartX-FinalX > (-0.5*vw) && StartX-FinalX < (1.2*vw)){
-                            if(StartY-FinalY < (-2.5*vw) && StartY-FinalY > (-7.5*vw)){
-                               down()
-                                document.body.ontouchmove = (event) =>{bubble(event)};
-                                $(e.target).css({
-                                    'border': 'none',
-                                    'border-radius': 'none'
-                                })
-                            } 
-                           }   
+    StartX = e.touches[0].clientX;
+    StartY = e.touches[0].clientY;
+    var allBall= document.getElementsByClassName('ball');
+    for(const x of allBall){
+        if(e.target == x){item = x;}}
+    document.body.ontouchmove =(ee) =>{
+        var FinalX = ee.touches[0].clientX;
+        var FinalY = ee.touches[0].clientY;
+     //   console.log(((StartY-FinalY)/screen.availWidth)*100 )
+        ///// LEFT ///////
+        if(StartX-FinalX > (5.3*vw) && StartX-FinalX < (10.5*vw)){
+            if(StartY-FinalY > (-1.4*vw) && StartY-FinalY < (2.2*vw)){
+                left()
+                document.body.ontouchmove = (event) =>{bubble(event)};
+                $(e.target).css({
+                    'border': 'none',
+                    'border-radius': 'none'
+                })
+            } 
+           }
+        ///// RIGHT ///////  
+      //  console.log(((StartX-FinalX)/screen.availWidth)*100 ) 
+        if(StartX-FinalX > (-7*vw) && StartX-FinalX < (-3*vw)){
+            if(StartY-FinalY > (-1.4*vw) && StartY-FinalY < (2.2*vw)){
+                right()
+                document.body.ontouchmove = (event) =>{bubble(event)};
+                $(e.target).css({
+                    'border': 'none',
+                    'border-radius': 'none'
+                })
+            } 
+           }
+           ///// UP ///////  
+        if(StartX-FinalX > (-0.5*vw) && StartX-FinalX < (1.2*vw)){
+            if(StartY-FinalY > (2.5*vw) && StartY-FinalY < (7.5*vw)){
+               up()
+                document.body.ontouchmove = (event) =>{bubble(event)};
+                $(e.target).css({
+                    'border': 'none',
+                    'border-radius': 'none'
+                })
+            } 
+           }
+        /////// DOWN ///////
+      //  console.log(((StartY-FinalY)/screen.availWidth)*100 )
+        if(StartX-FinalX > (-0.5*vw) && StartX-FinalX < (1.2*vw)){
+            if(StartY-FinalY < (-2.5*vw) && StartY-FinalY > (-7.5*vw)){
+               down()
+                document.body.ontouchmove = (event) =>{bubble(event)};
+                $(e.target).css({
+                    'border': 'none',
+                    'border-radius': 'none'
+                })
+            } 
+           }   
 
-                    }
-                }   
+    }
+}   
+
+               
